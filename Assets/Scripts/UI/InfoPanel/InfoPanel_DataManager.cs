b2be50dcaa2aee596cb704f91e9b738f4dff692c
Chris Proudmore <c.proudmore@gmail.com>
2019-04-12 07:59:44 +0000

    Basic working version of the Information panel, and its associated scripts.

---
+++ Assets/Scripts/UI/InfoPanel/InfoPanel_DataManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using MM.GLEAMoscopeVR.POIs;
+
+/// <summary>
+/// Script that manages the data displayed on the InfoPanel.
+/// </summary>
+public class InfoPanel_DataManager : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Title text object.")]
+    TextMeshProUGUI TitleText;
+    [SerializeField]
+    [Tooltip("Distance text object.")]
+    TextMeshProUGUI DistanceText;
+    [SerializeField]
+    [Tooltip("Description text object.")]
+    TextMeshProUGUI DescriptionText;
+    [SerializeField]
+    [Tooltip("Point of Interest Image object.")]
+    Image PointImage;
+
+    /// <summary>
+    /// Update the panel's display of information with a new point of interest.
+    /// Takes a PointOfInterest object, and utilises the Name, Distance, Description and Sprite parameters.
+    /// </summary>
+    /// <param name="point">The Point of Interest object whose parameters will be displayed on the panel.</param>
+    public void UpdateDisplay(PointOfInterest point)
+    {
+        TitleText.text = point.Name;
+        DistanceText.text = string.Format("Distance: {0}", point.Distance);
+        DescriptionText.text = point.Description;
+        PointImage.sprite = point.Sprite;
+    }
+}
---
+++ Assets/Scripts/UI/InfoPanel/InfoPanel_FaceCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPanel_FaceCamera : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The gameobject to look at. If not assigned, will default to the Main Camera in the scene.")]
+    GameObject ToLookAt;
+    [SerializeField]
+    [Tooltip("Whether the panel should face the camera. Toggle this programmatically with SetLookAtCamera()")]
+    bool LookAtCamera = false;
+
+    /// <summary>
+    /// On Awake, check to see if ToLookAt has been assigned in the editor. 
+    /// If it hasn't default to the scene's main camera.
+    /// </summary>
+    private void Awake()
+    {
+        if (ToLookAt == null)
+        {
+            ToLookAt = Camera.main.gameObject;
+        }
+    }
+
+    /// <summary>
+    /// Every frame, face the panel toward the ToLookAt gameobject.
+    /// This method by default faces an object AWAY from ToLookAt because of oddities with Canvas transforms.
+    /// </summary>
+    void LateUpdate()
+    {
+        if (LookAtCamera)
+        {
+            transform.LookAt(2 * transform.position - ToLookAt.transform.position);
+        }
+    }
+
+    public bool GetLookAtCamera()
+    {
+        return LookAtCamera;
+    }
+
+    public void SetLookAtCamera(bool value)
+    {
+        LookAtCamera = value;
+    }
+}
