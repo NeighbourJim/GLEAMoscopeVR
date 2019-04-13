c3c298de70a6393e5aa1217cfb7bfd5d224f74c3
Megan McLean <mmclean-dev@outlook.com>
2019-04-13 08:38:30 +0000

    rename InteractionMode to Experience mode to ensure conflicts with UnityEditor.InteractionMode namespace

---
+++ Assets/Scripts/Utility/Settings/ExperienceMode.cs
@@ -0,0 +1,29 @@
+﻿namespace GLEAMoscopeVR.Settings
+{
+    /// <summary>
+    /// Defines the interaction modes available to the user.
+    /// When set, functionality specific to the interaction mode will be enabled
+    /// and functionality specific to other modes will be disabled.
+    /// Currently, mixed interaction is excluded.
+    /// </summary>
+    [System.Serializable]
+    public enum ExperienceMode
+    {
+        /// <summary>
+        /// The user can interact with the POI nodes in the SKY ONLY.
+        /// POI nodes will be displayed on the map, but will not activate.
+        /// </summary>
+        Exploration,
+
+        /// <summary>
+        /// The user can interact with the POI nodes on the map at the WAR TABLE ONLY.
+        /// POI nodes will be displayed in they sky, but they will not activate.
+        /// </summary>
+        Passive,
+        
+        /// <summary>
+        /// To be decided.
+        /// </summary>
+        Mixed
+    }
+}
