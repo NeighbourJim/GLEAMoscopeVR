1c489803db745aa77d8732ad3cbf74fe07d93e93
Daniel Cherrin <daniel.r.cherrin@gmail.com>
2019-04-14 17:54:02 +0000

    + UIShaderRenderQueueAlter 	- Script that allows the UI/Default on canvas elements be overriden with a custom shader. Orginally implemented to solve the swirl overlapping the PointImage in the InfoPanel_Sky
    = Canvas_InfoPanel_Sky
    	= Background
    		+ Added UIShaderRenderQueueAlter script
    	= PointImage
    		+ Added UIShaderRenderQueueAlter script
    	= ImageBorder
    		+ Added UIShaderRenderQueueAlter script

---
+++ Assets/Scripts/UI/UIShaderRenderQueueAlter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIShaderRenderQueueAlter : MonoBehaviour
+{
+    //DC 2019/04/15
+    //Script to allow UI shaders to be overriden.
+    //Does not allow shader values/parameters to be changed from the script. Supplied shader's default values have to be changed.
+
+    public Shader shaderMat;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if(shaderMat != null)
+        {
+            Canvas.GetDefaultCanvasMaterial().shader = shaderMat;
+        }
+    }
+}
