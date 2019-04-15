20d1427af69da343a62790c65623f4e5caaa1af3
Daniel Cherrin <daniel.r.cherrin@gmail.com>
2019-04-15 11:26:04 +0000

    + RockFlatGroup 	- Empty Gameobject to organise/debloat rocks assets in Hierarchy.
    = Ground
    	- Shader Render Mode changed to Fade
    	
    = RockFlat_Atlas01
    	- Shader Render Mode changed to Fade
    
    + FadeAssetManager(*TEMPORARY)
    	- Handles asset Material/Sprite Fade on environment objects through a Coroutine.
    	- Toggled by OnClick() Unity Event on Canvas_Settings>InteractionModeSetting>ModeButton
    	
    = Ground (Material)
    	- Changed Albedo to 0,0,0
    	- Smoothness set to 0
    	- Specular Highlights set to False from True
    	- Reflections set to False from True

---
+++ Assets/FadeAssetController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeAssetController : MonoBehaviour
+{
+    //TEMP SCRIPT THIS NEEDS TO BE RE IMPLEMENTED PROPERLY
+
+    [SerializeField]
+    bool explortationMode = true;
+
+    [Header("GameObjects [Meshes]")]
+    [SerializeField]
+    GameObject[] meshObjects = null;
+
+    public void StartFade()
+    {
+        explortationMode = !explortationMode;
+
+
+        if(explortationMode)
+        {
+            if (meshObjects != null)
+            {
+                foreach (GameObject mesh in meshObjects)
+                {
+                    StartCoroutine(FadeUp(mesh));
+                }
+            }
+        }
+        else
+        {
+            print("Fade down");
+            if (meshObjects != null)
+            {
+                foreach (GameObject mesh in meshObjects)
+                {
+                    StartCoroutine(FadeDown(mesh));
+                }
+            }
+        }
+    }
+
+    IEnumerator FadeUp(GameObject pObject)
+    {
+        for(float i = 0; i < 1f; i += 0.023809f)
+        {
+            pObject.GetComponent<MeshRenderer>().material.color = new Color(1f, 1f, 1f, i);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    IEnumerator FadeDown(GameObject pObject)
+    {
+
+        for (float i = 0f; i <= 1f; i += 0.023809f)
+        {
+            print("into Fade down");
+            pObject.GetComponent<MeshRenderer>().material.color = new Color(1f, 1f, 1f, 1-i);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
+
+/*
+void UpdateProxReticleObjAlpha()
+        {
+            if (reticleObject != null)
+            {
+
+                reticleObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, reticleAlpha / 255f);
+
+                if (doesReticleHideOnNoHit &&
+                    currentCentreHitObject == null && currentProxHitObject == null)
+                {
+                    reticleObject.GetComponent<SpriteRenderer>().enabled = false;
+                }
+                else
+                {
+                    reticleObject.GetComponent<SpriteRenderer>().enabled = true;
+                }
+            }
+        }
+*/
+
+/*
+ IEnumerator FadeUp(GameObject sphere)
+        {
+            ToggleRendererState(sphere, false);//MM - sets renderer active
+            fadingUp = true;
+            
+            for (float i = 0; i < 1.05f; i += 0.025f)
+            {
+                sphere.GetComponent<Renderer>().material.SetFloat("_Transparency", i);
+                yield return new WaitForSeconds(0.05f);
+            }
+
+            fadingUp = false;
+        }
+
+        /// <summary>
+        /// Simple co-routine to fade the alpha down on a sky sphere.
+        /// </summary>
+        /// <param name="sphere">The game object to be faded. The shader for the object must have a property named "_Transparency" tied to the alpha channel!</param>
+        /// <returns></returns>
+        IEnumerator FadeDown(GameObject sphere)
+        {
+            fadingDown = true;
+            for (float i = 0; i < 1.05f; i += 0.025f)
+            {
+                sphere.GetComponent<Renderer>().material.SetFloat("_Transparency", 1f - i);
+                yield return new WaitForSeconds(0.05f);
+            }
+
+            ToggleRendererState(sphere, true);//MM - sets renderer inactive
+            fadingDown = false;
+        }
+*/
