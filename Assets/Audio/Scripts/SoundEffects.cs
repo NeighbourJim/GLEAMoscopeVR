03d5b14d7fc4258c95e8e74a7deeee390690df45
Gal Rose <galiboy387@hotmail.com>
2019-05-20 07:50:57 +0000

    GLEAMoscopeVR-Proto -> Now includes voiceover audioclips

--- Assets/Audio/Scripts/SoundEffects.cs
+++ Assets/Audio/Scripts/SoundEffects.cs
@@ -4,18 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+/// <summary>
+/// This manager script allows all AudioClips to be stored into an array via class inheritance,
+/// eliminating the need to manually apply an AudioSource component to every gameobject via the inspector.
+/// </summary>
+
 public class SoundEffects : MonoBehaviour
 {
     public Sound[] sounds;
-    public static SoundEffects instance = null;
+    //public static SoundEffects instance = null;
 
     void Awake()
     {
         
-        // creates an audiomanager
         foreach(Sound sound in sounds)
         {
             sound.source = gameObject.AddComponent<AudioSource>();
+
             sound.source.clip = sound.clip;
 
             sound.source.volume = sound.volume;
@@ -25,7 +30,6 @@ public class SoundEffects : MonoBehaviour
 
     public void Play (string name)
     {
-        
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.PlayOneShot(s.clip, 0.7f);
     }
