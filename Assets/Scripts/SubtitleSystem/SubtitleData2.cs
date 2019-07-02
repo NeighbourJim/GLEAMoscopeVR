14c9ed4b8dfdd88e2f67bd3b5a833cb57a30843f
Megan McLean <mmclean-dev@outlook.com>
2019-07-02 04:06:25 +0000

    Second option (Subtitle data)

---
+++ Assets/Scripts/SubtitleSystem/SubtitleData2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GLEAMoscopeVR.SubtitleSystem
+{
+    /// <summary>
+    /// PURPOSE OF SCRIPT GOES HERE 
+    /// </summary>
+    [CreateAssetMenu(menuName = "GLEAMoscopeVR/SubtitleData2", fileName = "New SubtitleData2")]
+    public class SubtitleData2 : ScriptableObject
+    {
+        public float startDelay = 0;
+        [TextArea] public List<string> subtitle = new List<string>();
+        public List<float> delayLengthF = new List<float>();
+        public List<float> delayLengthM = new List<float>();
+    }
+}
