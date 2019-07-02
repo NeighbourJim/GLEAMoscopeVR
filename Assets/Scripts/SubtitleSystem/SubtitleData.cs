2940967f097374657c9a40568c8866bac073babf
Megan McLean <mmclean-dev@outlook.com>
2019-07-02 04:03:08 +0000

    = Subtitle data

---
+++ Assets/Scripts/SubtitleSystem/SubtitleData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace GLEAMoscopeVR.SubtitleSystem
+{
+    [CreateAssetMenu(menuName = "GLEAMoscopeVR/Subtitle Data", fileName = "New Subtitle Data")]
+    public class SubtitleData : ScriptableObject
+    {
+        public float startDelay = 0;
+        [TextArea]
+        public string[] subtitle;
+        public float[] delayLengthF;
+        public float[] delayLengthM;
+    }
+}
