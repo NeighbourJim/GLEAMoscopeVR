758ac1e6d5cc90d9adc29186e58526177a166a7c
NeighbourJim <c.proudmore@gmail.com>
2019-11-05 16:17:30 +0800

    Fixed language setting not maintaining between launches.

--- Assets/Scripts/UI/TranslationTextChanger.cs
+++ Assets/Scripts/UI/TranslationTextChanger.cs
@@ -8,6 +8,7 @@ public class TranslationTextChanger : MonoBehaviour
 {
     private TextMeshProUGUI textfieldUGUI = null;
     private TextMeshPro textfieldGeneric = null;
+    LanguageSetting CurrentLanguage = LanguageSetting.English;
 
     [Tooltip("The text this object will display when in English mode"), TextArea(3, 10)]
     [SerializeField] private string englishText;
@@ -16,21 +17,24 @@ public class TranslationTextChanger : MonoBehaviour
 
     private void ChangeText()
     {
-        var CurrentLanguage = SettingsManager.Instance.CurrentLanguageSetting;
-        switch (CurrentLanguage)
+        if (CurrentLanguage != SettingsManager.Instance.CurrentLanguageSetting)
         {
-            case LanguageSetting.English:
-                if (textfieldUGUI != null)
-                    textfieldUGUI.text = englishText;
-                else
-                    textfieldGeneric.text = englishText;
-                break;
-            case LanguageSetting.Italian:
-                if (textfieldUGUI != null)
-                    textfieldUGUI.text = italianText;
-                else
-                    textfieldGeneric.text = italianText;
-                break;
+            CurrentLanguage = SettingsManager.Instance.CurrentLanguageSetting;
+            switch (CurrentLanguage)
+            {
+                case LanguageSetting.English:
+                    if (textfieldUGUI != null)
+                        textfieldUGUI.text = englishText;
+                    else
+                        textfieldGeneric.text = englishText;
+                    break;
+                case LanguageSetting.Italian:
+                    if (textfieldUGUI != null)
+                        textfieldUGUI.text = italianText;
+                    else
+                        textfieldGeneric.text = italianText;
+                    break;
+            }
         }
     }
 
@@ -39,15 +43,22 @@ public class TranslationTextChanger : MonoBehaviour
         SetAndCheckReferences();
     }
 
+    private void LateUpdate()
+    {
+        ChangeText();
+    }
+
     private void OnEnable()
     {
         EventManager.Instance.AddListener<TeleportToSceneEvent>(_ => ChangeText());
+        EventManager.Instance.AddListener<VideoClipEndedEvent>(_ => ChangeText());        
         EventManager.Instance.AddListener<LanguageSettingChangedEvent>(_ => ChangeText());
     }
 
     private void OnDisable()
     {
         EventManager.Instance.RemoveListener<TeleportToSceneEvent>(_ => ChangeText());
+        EventManager.Instance.AddListener<VideoClipEndedEvent>(_ => ChangeText());
         EventManager.Instance.RemoveListener<LanguageSettingChangedEvent>(_ => ChangeText());
     }
 
